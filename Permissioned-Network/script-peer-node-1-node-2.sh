44c1fb20f0af5185af2c54d6b66ed57ac14228b0
Jose E. Martinez <josemartinezchevez@gmail.com>
2023-06-12 20:00:50 -0600

    chore: adding script for configuration

---
+++ Permissioned-Network/script-peer-node-1-node-2.sh
@@ -0,0 +1,1 @@
+curl -X POST --data '{"jsonrpc":"2.0","method":"admin_addPeer","params":["enode://ef609cbb193611ad1c9d25f19b3674cc0d4452a6063ccdae1e1424129b75f4ad298dc50e2c2232efe9ce8e12a854165611d6f7c39189d0f34122a7d280500e63@127.0.0.1:30303"],"id":1}' http://127.0.0.1:8546
